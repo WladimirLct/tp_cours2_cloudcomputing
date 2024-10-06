6a1a9ab2ab1f7503d762e62d42e9ccce1462d717
WladimirLct <wlad.lucet@gmail.com>
2024-10-06 22:26:55 +0200

    Fix db + Readme

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# Endpoints
+- ``/`` Hello World
+- ``/insert_data`` To insert data in the mongoDB and create a bucket
+- ``/get_data`` To gather the mongoDB data
+
+The last two endpoints are just placeholders, they do not really work (no calls to mongoDB or the buckets).
--- terraform/main.tf
+++ terraform/main.tf
@@ -9,8 +9,27 @@ resource "azurerm_resource_group" "api" {
     name     = "course-2-rg-${random_string.random.result}"
 }
 
+resource "azurerm_storage_account" "storage" {
+  name                     = "course2storage${random_string.random.result}"
+  resource_group_name      = azurerm_resource_group.api.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  
+  depends_on = [ 
+    azurerm_resource_group.api
+  ]
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "blob"  # Allows public read access to blobs
+}
+
+
 resource "azurerm_cosmosdb_account" "cosmos_acc" {
-  name                = "course-2-${random_string.random.result}"
+  name                = "course-2"
   location            = var.location
   resource_group_name = "${azurerm_resource_group.api.name}"
   offer_type          = "Standard"
@@ -42,27 +61,16 @@ resource "azurerm_cosmosdb_mongo_database" "cosmos_db" {
 }
 
 
-resource "azurerm_storage_account" "storage" {
-  name                     = "course2storage${random_string.random.result}"
-  resource_group_name      = azurerm_resource_group.api.name
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource "azurerm_storage_container" "storage_container" {
-  name                  = "content"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "blob"  # Allows public read access to blobs
-}
-
-
 resource "azurerm_service_plan" "api" {
     name                = "webapp-service-plan"
     resource_group_name = azurerm_resource_group.api.name
     location            = azurerm_resource_group.api.location
     os_type             = "Linux"
     sku_name            = "B1"
+
+    depends_on = [ 
+        azurerm_resource_group.api
+    ]
 }
 
 resource "azurerm_linux_web_app" "api" {
